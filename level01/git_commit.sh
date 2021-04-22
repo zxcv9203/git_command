a871d13440aa8046bc4d33a084407a909b90ffc2
zxcv9203 <zxcv9203@naver.com>
2021-04-23 08:16:51 +0900

    commit needs

---
+++ level01/git_commit.sh
@@ -0,0 +1,8 @@
+# 현재 디렉터리의 hello.c 라는 파일을 이미지에 추가합니다.
+git add hello.c
+
+# 아직 commit되지 않은 파일들을 확인할 수 있는 명령입니다. 
+git status
+
+# add를 이용해서 추가한 파일을 commit 하고 "hello insert"라는 메세지를 기록해둡니다.
+git commit -m "hello insert"
